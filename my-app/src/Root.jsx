1de195963a6888d951883a5758fcca451719510a
Luke Cherniawski <lukecherniawski@Lukes-MacBook-Pro.local>
2026-03-07 00:21:30 -0700

    I am finally done

--- my-app/src/Root.jsx
+++ my-app/src/Root.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-import { Outlet, NavLink, useNavigate, Navigate } from "react-router-dom";
+import React, {useEffect} from "react";
+import { Outlet, NavLink, useNavigate, useLocation } from "react-router-dom";
 
 export default function Root(){
+
+   function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 
     const navigate = useNavigate();
@@ -13,17 +21,18 @@ export default function Root(){
             <h2>Welcome To Black Diamond Mountain Page For Whitefish</h2>
             <nav>
                 <ul>
-                    <NavLink to="/"><li>Welcome Page</li></NavLink>
-                    <NavLink to="HaskillSlide"><li>Haskill Slide</li></NavLink>
-                    <NavLink to="NBC"><li>North Bowl Chute</li></NavLink>
-                    <NavLink to="Bighorn"><li>Bighorn</li></NavLink>
-                    <NavLink to="PictureChutes"><li>Picture Chutes</li></NavLink>
-                    <NavLink to="EastRim"><li>East Rim Chutes</li></NavLink>
+                    <NavLink className={({ isActive }) => isActive? 'activeNavLink' : 'inactiveNavLink'} to="/"><li>Welcome Page</li></NavLink>
+                    <NavLink className={({ isActive }) => isActive? 'activeNavLink' : 'inactiveNavLink'} to="HaskillSlide"><li>Haskill Slide</li></NavLink>
+                    <NavLink className={({ isActive }) => isActive? 'activeNavLink' : 'inactiveNavLink'} to="NBC"><li>North Bowl Chute</li></NavLink>
+                    <NavLink className={({ isActive }) => isActive? 'activeNavLink' : 'inactiveNavLink'} to="Bighorn"><li>Bighorn</li></NavLink>
+                    <NavLink className={({ isActive }) => isActive? 'activeNavLink' : 'inactiveNavLink'} to="PictureChutes"><li>Picture Chutes</li></NavLink>
+                    <NavLink className={({ isActive }) => isActive? 'activeNavLink' : 'inactiveNavLink'} to="EastRim"><li>East Rim Chutes</li></NavLink>
                 </ul>
             </nav>
             </header>
         </div>
         <div id="main">
+            <ScrollToTop />
             <Outlet />
 
         </div>
